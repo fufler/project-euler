4fe5d3e25268f51a1972d4d77ffe0ccfd9a9d02a
Alexey Ermakov <fufler@gmail.com>
2019-05-26 22:15:23 +0300

    Simplify solution for 004

--- 001-050/004-largest-palindrome-product.hs
+++ 001-050/004-largest-palindrome-product.hs
@@ -1,13 +1,4 @@
-get_digits :: Int -> [Int] -> [Int]
-get_digits n acc
-    | n == 0    = acc
-    | otherwise = get_digits (n `div` 10) ((n `rem` 10):acc)
-
-is_palindrome :: Int -> Bool
-is_palindrome n = digits == reverse digits
-    where digits = get_digits n []
-
 solve :: Int
-solve = maximum [a * b | a <- [100..999], b <- [100..999], is_palindrome (a * b)]
+solve = maximum [a * b | a <- [100..999], b <- [100..999], let p = show $ a * b, p == reverse p]
 
 main = print solve
